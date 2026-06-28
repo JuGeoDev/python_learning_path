a920f995ed64af60f63bdf62c6d81be80399c2a3
JuGeoDev <jugeorg@proton.me>
2026-06-28 19:43:42 +0200

    Added text input fields. Also learned you can not use pack() and grid() at the same time

--- basics/hello_world_gui.py
+++ basics/hello_world_gui.py
@@ -8,13 +8,25 @@ root.title("This title is in the top bar!")
 # Widgets are added here
 
 #Label of main window = "Title" at the top center
-label = tk.Label(root, text="Hello label!")
+#label = tk.Label(root, text="Hello label!")
 #Label gets placed *inside* window
-label.pack()
+#label.pack() -> However you can not use pack AND grid at the same time because then the compiler will be confused where the packed object shoulld go if not specified
 
 #Creates button inside the window. Args = Which window, text, size, funtion of button
 button = tk.Button(root, text="Click to destroy", width=25, command=root.destroy)
-button.pack()
+button.grid(row=0, column=1)
+
+#Creates labels inside window and sorts them in a grid
+tk.Label(root, text="First Name").grid(row=1, column=0)
+tk.Label(root, text="Last Name").grid(row=2, column=0)
+
+#Accepts single line user input
+entry1 = tk.Entry(root)
+entry2 = tk.Entry(root)
+
+#Places entry fields next to labels from above in grid
+entry1.grid(row=1, column=1)
+entry2.grid(row=2, column=1)
 
 #Next line starts main loop and keeps window responsive
 root.mainloop()
