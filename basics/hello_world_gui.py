fc300de491034dcdd5f2d626bd669237e97377c8
JuGeoDev <jugeorg@proton.me>
2026-06-28 19:53:50 +0200

    Tried out color changes of elements

--- basics/hello_world_gui.py
+++ basics/hello_world_gui.py
@@ -4,6 +4,8 @@ import tkinter as tk
 root = tk.Tk()
 #Title = Text in the top bar where the close,max,min buttons are
 root.title("This title is in the top bar!")
+#Configure window. E.g. color
+root.configure(bg='lightblue')
 
 # Widgets are added here
 
@@ -15,10 +17,11 @@ root.title("This title is in the top bar!")
 #Creates button inside the window. Args = Which window, text, size, funtion of button
 button = tk.Button(root, text="Click to destroy", width=25, command=root.destroy)
 button.grid(row=0, column=1)
+button.configure(bg='white')
 
 #Creates labels inside window and sorts them in a grid
-tk.Label(root, text="First Name").grid(row=1, column=0)
-tk.Label(root, text="Last Name").grid(row=2, column=0)
+firstname_label = tk.Label(root, bg='white', text="First Name").grid(row=1, column=0)
+lastname_label = tk.Label(root, bg='white', text="Last Name").grid(row=2, column=0)
 
 #Accepts single line user input
 entry1 = tk.Entry(root)
