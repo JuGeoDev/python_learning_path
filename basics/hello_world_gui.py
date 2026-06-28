b058eb87e78c5e75d99deffdf69972a993afe10e
JuGeoDev <jugeorg@proton.me>
2026-06-28 19:29:28 +0200

    Added title to window

--- basics/hello_world_gui.py
+++ basics/hello_world_gui.py
@@ -2,10 +2,12 @@ import tkinter as tk
 
 #Next line creates main window
 root = tk.Tk()
+root.title("This title is in the top bar!")
 # Widgets are added here
 
 #Label of main window = "Title" at the top center
 label = tk.Label(root, text="Hello label!")
+#Label gets placed *inside* window
 label.pack()
 
 #Next line starts main loop and keeps window responsive
