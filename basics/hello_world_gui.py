52593005638c547fbab2f8bbefc4d8648a284882
JuGeoDev <jugeorg@proton.me>
2026-06-28 19:27:05 +0200

    Added label to window

--- basics/hello_world_gui.py
+++ basics/hello_world_gui.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
+#Next line creates main window
 root = tk.Tk()
 # Widgets are added here
 
+#Label of main window = "Title" at the top center
+label = tk.Label(root, text="Hello label!")
+label.pack()
+
+#Next line starts main loop and keeps window responsive
 root.mainloop()
