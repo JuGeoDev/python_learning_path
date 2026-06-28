c1293a214719ef60c3e0a4002b6d3b6e7af68079
JuGeoDev <jugeorg@proton.me>
2026-06-28 22:16:22 +0200

    Added basic calculator logic. Don't know if these will work

--- basics/calculator/calculator_gui.py
+++ basics/calculator/calculator_gui.py
@@ -18,22 +18,22 @@ emptyline = tk.Label(frame, width=root_standardwidth, bg='#FFBBFF').grid(row=0, 
 emptyline = tk.Label(frame, width=root_standardwidth, bg='#FFBBFF').grid(row=6, column=7)
 
 #Buttons 1-9
-button_one = tk.Button(frame, text="1", bg='white', width=root_standardwidth, command=root.destroy).grid(row=1, column=1)
-button_two = tk.Button(frame, text="2", bg='white', width=root_standardwidth, command=root.destroy).grid(row=1, column=2)
-button_three = tk.Button(frame, text="3", bg='white', width=root_standardwidth, command=root.destroy).grid(row=1, column=3)
-button_four = tk.Button(frame, text="4", bg='white', width=root_standardwidth, command=root.destroy).grid(row=2, column=1)
-button_five = tk.Button(frame, text="5", bg='white', width=root_standardwidth, command=root.destroy).grid(row=2, column=2)
-button_six = tk.Button(frame, text="6", bg='white', width=root_standardwidth, command=root.destroy).grid(row=2, column=3)
-button_seven = tk.Button(frame, text="7", bg='white', width=root_standardwidth, command=root.destroy).grid(row=3, column=1)
-button_eight = tk.Button(frame, text="8", bg='white', width=root_standardwidth, command=root.destroy).grid(row=3, column=2)
-button_nine = tk.Button(frame, text="9", bg='white', width=root_standardwidth, command=root.destroy).grid(row=3, column=3)
+button_one = tk.Button(frame, text="1", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=1, column=1)
+button_two = tk.Button(frame, text="2", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=1, column=2)
+button_three = tk.Button(frame, text="3", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=1, column=3)
+button_four = tk.Button(frame, text="4", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=2, column=1)
+button_five = tk.Button(frame, text="5", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=2, column=2)
+button_six = tk.Button(frame, text="6", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=2, column=3)
+button_seven = tk.Button(frame, text="7", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=3, column=1)
+button_eight = tk.Button(frame, text="8", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=3, column=2)
+button_nine = tk.Button(frame, text="9", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=3, column=3)
 
 #Buttons operators
-button_plus = tk.Button(frame, text="+", bg='white', width=root_standardwidth, command=root.destroy).grid(row=1, column=5)
-button_minus = tk.Button(frame, text="-", bg='white', width=root_standardwidth, command=root.destroy).grid(row=1, column=6)
-button_multiply = tk.Button(frame, text="*", bg='white', width=root_standardwidth, command=root.destroy).grid(row=2, column=5)
-button_divide = tk.Button(frame, text="/", bg='white', width=root_standardwidth, command=root.destroy).grid(row=2, column=6)
-button_equals = tk.Button(frame, text="=", bg='white', width=root_standardwidth, command=root.destroy).grid(row=3, column=5)
+button_plus = tk.Button(frame, text="+", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=1, column=5)
+button_minus = tk.Button(frame, text="-", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=1, column=6)
+button_multiply = tk.Button(frame, text="*", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=2, column=5)
+button_divide = tk.Button(frame, text="/", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=2, column=6)
+button_equals = tk.Button(frame, text="=", bg='white', activebackground="#F8E7F8", width=root_standardwidth, command=root.destroy).grid(row=3, column=5)
 
 #Add empty row in grid to seperate buttons from input
 emptyline = tk.Label(frame, width=root_standardwidth, bg='#FFBBFF').grid(row=4, column=1)
